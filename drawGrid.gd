3c80c83abdf27b34e7d295909526eb367972c426
skits4107 <74423228+skits4107@users.noreply.github.com>
2024-05-29 17:11:22 -0700

    added and fixed comments

--- drawGrid.gd
+++ drawGrid.gd
@@ -15,18 +15,22 @@ func _ready():
 
 
 func _draw():
-	#draw all of the vertical lines
+	#draw all of the horizontal lines
 	for y in range(-height, height):
+		#draw line from left to right for current y
 		draw_line(Vector2((-width) * tile_size, y * tile_size), Vector2(width * tile_size, y * tile_size), color, line_width)
 	
-	#draw all of the horizontal lines
+	#draw all of the vertical lines
 	for x in range(-width, width):
+		#draw line from top to bottom for current x
 		draw_line(Vector2(x * tile_size, (-height) * tile_size), Vector2(x * tile_size, height * tile_size), color, line_width)
 				
 
 func _process(delta):
 	if camera != null:
+		#by adjsting the line size to be inversely proportional to the zoom i ensure  the lines always display properly
 		line_width = 1.2 / camera.zoom.x
+		
 	queue_redraw()
 
 
